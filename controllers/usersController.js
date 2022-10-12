3c6279dd427461fc3763985cc9dca04344866022
dounya-m <mellouki.dounya@gmail.com>
2022-10-12 10:47:30 +0100

    update connecting with db

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,23 @@
 const asyncHandler = require('express-async-handler')
+const User = require('../models/usersModel')
 
 const getUsers =  asyncHandler(async(req, res) => {
-    res.status(200).json({ message:'get users'});
-});
+    const users = await User.find()
+
+    res.status(200).json(users);
+}); 
 
 const postUsers =  asyncHandler(async(req, res) => {
-    if(!req.body.text){
+    if(!req.body.name || !req.body.email || !req.body.password) {
         res.status(400).json({ message:'text is required'});
     };
-    res.status(200).json({ message:'post users'});
+    const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    res.status(200).json(user);
 });
 
 const setUser = asyncHandler(async(req, res) => {
