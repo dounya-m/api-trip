8451f9cd7dea9323a2c74e6899fa5b376d32f692
dounya-m <mellouki.dounya@gmail.com>
2022-10-13 10:03:11 +0100

    create user/login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt');
 const User = require('../models/usersModel')
 
 const getUsers =  asyncHandler(async(req, res) => {
@@ -16,6 +17,10 @@ const postUsers =  asyncHandler(async(req, res) => {
         email: req.body.email,
         password: req.body.password,
     });
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
 
     res.status(200).json(user);
 });
@@ -32,8 +37,37 @@ const updateUser = asyncHandler(async(req, res) => {
     res.status(200).json(updateUser);
 });
 
+const deleteUser = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.params.id);
+    if(!user){
+        res.status(400)
+        throw new Error('User not found');
+    }
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'User deleted'});
+
+});
+
+//create login method
+const login = asyncHandler(async(req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if(!user){
+        res.status(400)
+        throw new Error('User not found');
+    };
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json(user)
+    } else {
+        res.status(401)
+        throw new Error('Invalid email or password')
+    }
+})
+
 module.exports = {
     getUsers,
     postUsers,
     updateUser,
+    deleteUser,
+    login,
 };
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, postUsers, updateUser } = require('../controllers/usersController');
+const { getUsers, postUsers, updateUser, deleteUser, login } = require('../controllers/usersController');
 
 router.route('/').get(getUsers).post(postUsers);
-router.route('/:id').put(updateUser);
+router.route('/:id').put(updateUser).delete(deleteUser);
+router.route('/login').post(login);
 
 
 module.exports = router;
